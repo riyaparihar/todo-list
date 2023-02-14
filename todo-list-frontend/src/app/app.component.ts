91ac2b06f262e8bebdf1a712ad0c66771db410c9
riyap2000 <riya@expansionjs.com>
2023-02-14 09:52:04 +0530

    Implement search - frontend only

--- todo-list-frontend/src/app/app.component.ts
+++ todo-list-frontend/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import {Todo, TodoService} from "./todo.service";
 import {Observable} from "rxjs";
+import { debounceTime, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -12,18 +13,34 @@ import {Observable} from "rxjs";
     </div>
     <div class="list">
       <label for="search">Search...</label>
-      <input id="search" type="text">
-      <app-progress-bar  *ngIf="!(todos$|async)?.length"></app-progress-bar>
+      <input id="search" type="text" #searchInput (keyup)="handleSearch(searchInput.value)">
+      <app-progress-bar  *ngIf="!(todos$|async)?.length && !hasTodosLoaded"></app-progress-bar>
       <app-todo-item *ngFor="let todo of todos$ | async" [item]="todo"></app-todo-item>
+      <div class="no-match" *ngIf="!(todos$|async)?.length && hasTodosLoaded">
+         No match found!
+      </div>
     </div>
   `,
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
 
-  readonly todos$: Observable<Todo[]>;
+  todos$: Observable<Todo[]>;
+  hasTodosLoaded: boolean = false;
 
-  constructor(todoService: TodoService) {
+  constructor(private todoService: TodoService) {
     this.todos$ = todoService.getAll();
+  }
+
+  handleSearch(query: string): void {
+    this.hasTodosLoaded = false;
+    this.todos$ = this.todoService.getAll().pipe(
+      debounceTime(200),
+      map((todos: Todo[]) => {
+        this.hasTodosLoaded = true;
+        return todos.filter((todo: Todo) => todo.task.toLowerCase().includes(query.toLowerCase())
+        );
+      })
+    )
   }
 }
